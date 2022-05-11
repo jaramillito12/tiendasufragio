9e76555794d92a157d50dc147ff1e15e47ce0407
jaramillo <jaramillito098x@gmail.com>
2022-05-11 17:26:13 -0500

    index

--- controlador.js
+++ controlador.js
@@ -3,106 +3,142 @@ let producto1={
     nombre:"producto1",
     precio:500000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-bbed4.appspot.com/o/64087-1.jpg?alt=media&token=b0371205-78a7-4e7b-a0a0-1ebbc0ceddb5"
 }
 
 let producto2={
     nombre:"producto2",
     precio:500000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-bbed4.appspot.com/o/aaaaaaaaaa.jpg?alt=media&token=85a0e0b6-4a26-419c-a2c2-407f6c4e74b0"
 }
 
 let producto3={
     nombre:"producto3",
     precio:500000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-bbed4.appspot.com/o/arbelaez.jpg?alt=media&token=08fa3ced-f0d0-4995-be4c-07db1f6f6ea4"
 }
 
 let producto4={
     nombre:"producto4",
     precio:500000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-bbed4.appspot.com/o/camila.jpg?alt=media&token=77165010-0ed5-4ffd-8629-1d7665bdee34"
 }
 
 let producto5={
     nombre:"producto5",
     precio:500000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-bbed4.appspot.com/o/eeeeeeeeeeeeeee.jpg?alt=media&token=30c7ca1a-2fc2-4bc7-bdf8-4be40895d2bc"
 }
 
 let producto6={
     nombre:"producto6",
     precio:500000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-bbed4.appspot.com/o/giraldo.jpg?alt=media&token=ba5b4d72-ec95-4761-bebf-3ac1d30e188d"
 }
 
 let producto7={
     nombre:"producto7",
     precio:500000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-bbed4.appspot.com/o/hjuhjuhjuhju.jpg?alt=media&token=ce20ba8a-9985-4cbc-8bdf-fbf59df40d13"
 }
 
 let producto8={
     nombre:"producto8",
     precio:500000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-bbed4.appspot.com/o/iiiiiiiiiiiiiii.jpg?alt=media&token=dc0e0d46-8f42-4f9c-8d22-a156ba940bb3"
 }
 
 let producto9={
     nombre:"producto9",
     precio:500000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-bbed4.appspot.com/o/jijijiji.jpg?alt=media&token=83fee508-53df-4c91-ae43-642c8ea8cf1c"
 }
 
 let producto10={
     nombre:"producto10",
     precio:500000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-bbed4.appspot.com/o/kokok.jpg?alt=media&token=950c78dc-8327-45df-bc5a-f020157b0d3c"
 }
 
 let producto11={
     nombre:"producto11",
     precio:500000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-bbed4.appspot.com/o/nooooooooooouuuuuu.jpg?alt=media&token=e9f421bd-e807-460b-9d49-451083ef95fc"
 }
 
 let producto12={
     nombre:"producto12",
     precio:500000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-bbed4.appspot.com/o/nubia.jpg?alt=media&token=bfbb4442-2427-4057-807d-8f256ca16b67"
 }
 
 let producto13={
     nombre:"producto13",
     precio:500000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-bbed4.appspot.com/o/oooooooooooooo.jpg?alt=media&token=7fde0082-95de-4e46-bfff-2d0b55ee8d18"
 }
 
 let producto14={
     nombre:"producto14",
     precio:500000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-bbed4.appspot.com/o/popopo.jpg?alt=media&token=07e006cf-b315-4caf-8102-be5c1f6dac9a"
 }
 
 let producto15={
     nombre:"producto15",
     precio:500000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-bbed4.appspot.com/o/sandra.jpg?alt=media&token=44705e2a-de92-4956-96b0-490fd97ba0fc"
 }
+
+let producto16={
+    nombre:"producto16",
+    precio:500000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-bbed4.appspot.com/o/sisisis.jpg?alt=media&token=a16e97ff-f637-4e77-a0b3-bfe27439fef3"
+}
+
+let producto17={
+    nombre:"producto17",
+    precio:500000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-bbed4.appspot.com/o/tutututut.jpg?alt=media&token=1e50f614-f75e-47b2-abbc-d4dd1c501109"
+}
+
+let producto18={
+    nombre:"producto18",
+    precio:500000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-bbed4.appspot.com/o/yuyuyu.jpg?alt=media&token=8d7e8727-b3b7-471c-90bf-1e1aa418327f"
+}
+
+let producto19={
+    nombre:"producto19",
+    precio:500000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-bbed4.appspot.com/o/%C3%B1oo%C3%B1%C3%B1oo.jpg?alt=media&token=d25234db-8230-44d4-871d-274deb1e2f99"
+}
+
+let producto20={
+    nombre:"producto20",
+    precio:500000,
+    estado:"Nuevo",
+    foto:"https://firebasestorage.googleapis.com/v0/b/latiendita-bbed4.appspot.com/o/uuuuuuuuuu.jpg?alt=media&token=4dcdbfdd-287f-4ad0-9930-e800d117fcd4"
+}
+
 
 //ARREGLO
 let productos=Array(
@@ -120,11 +156,38 @@ let productos=Array(
     producto12,
     producto13,
     producto14,
-    producto15
+    producto15,
+    producto16,
+    producto17,
+    producto18,
+    producto19,
+    producto20,
 )
 
 //RECORRO EL ARREGLO
+let contenedor= document.getElementById("contenedor")
 productos.forEach(function(producto){
-    console.log(producto.nombre)
+
+    let foto=document.createElement("img")
+    foto.classList.add("w-100","img-fluid")
+    foto.src=producto.foto
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card")
+    tarjeta.appendChild(foto)
+    columna.appendChild(tarjeta)
+    contenedor.appendChild(columna)
+
+
+
+
+
+
+
+
+
+
+    
 })
 
---
+++ firebase.js
@@ -0,0 +1,23 @@
+/*
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCSs9CZTjIw_2RGLsLZjXcg_QLRomj5dQk",
+    authDomain: "la-tiendita-29138.firebaseapp.com",
+    projectId: "la-tiendita-29138",
+    storageBucket: "la-tiendita-29138.appspot.com",
+    messagingSenderId: "145831237487",
+    appId: "1:145831237487:web:1dba4fa3c19c36a9bb3a9a",
+    measurementId: "G-ET18Q3F2TM"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+*/
